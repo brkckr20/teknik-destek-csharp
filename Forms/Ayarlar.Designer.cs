9523fcda84231e952fd45d4c9dbac98c26ed86cd
brkckr20 <burak2043@gmail.com>
2025-03-10 13:50:15 +0300

    yedekleme işlemi çözüldü

--- Forms/Ayarlar.Designer.cs
+++ Forms/Ayarlar.Designer.cs
@@ -90,6 +90,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Yedekleme Adresi";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnYedekle
--- Forms/Ayarlar.cs
+++ Forms/Ayarlar.cs
@@ -25,57 +25,59 @@ namespace Talepler.Forms
         }
         private void LoadBackupPath()
         {
-            try
-            {
-                // path.txt dosyasından yolu oku ve TextBox'a yazdır
-                if (File.Exists("C:\\Users\\casper\\Desktop\\ExtremeTalepler\\ExtremeTalepler\\Talepler\\BackupPath.txt"))
-                {
-                    string savedPath = File.ReadAllText("C:\\Users\\casper\\Desktop\\ExtremeTalepler\\ExtremeTalepler\\Talepler\\BackupPath.txt");
-                    string backupFileName = "yedek_" + DateTime.Now.ToString("dd.MM.yyyy") + ".bak";
-                    textBox1.Text = savedPath + "yedek_" + backupFileName;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Yedekleme yolu yüklenirken bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            textBox1.Text = "C:\\Users\\casper\\Desktop\\ExtremeTalepler\\ExtremeTalepler";
+            //try
+            //{
+            //    // path.txt dosyasından yolu oku ve TextBox'a yazdır
+            //    if (File.Exists("C:\\Users\\casper\\Desktop\\ExtremeTalepler\\ExtremeTalepler\\Talepler\\BackupPath.txt"))
+            //    {
+            //        string savedPath = File.ReadAllText("C:\\Users\\casper\\Desktop\\ExtremeTalepler\\ExtremeTalepler\\Talepler\\BackupPath.txt");
+            //        string backupFileName = "yedek_" + DateTime.Now.ToString("dd.MM.yyyy") + ".bak";
+            //        textBox1.Text = savedPath + "yedek_" + backupFileName;
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show($"Yedekleme yolu yüklenirken bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
         }
         private void SaveBackupPath(string path)
         {
-            try
-            {
-                // Path.txt dosyasına yolu kaydet
-                File.WriteAllText("path.txt", path);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Yedekleme yolu kaydedilirken bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            //try
+            //{
+            //    // Path.txt dosyasına yolu kaydet
+            //    File.WriteAllText("path.txt", path);
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show($"Yedekleme yolu kaydedilirken bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // SaveFileDialog ile dosya yolu seçme
-            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
-            {
-                saveFileDialog.Filter = "SQL Server Backup Files (*.bak)|*.bak";
+            //// SaveFileDialog ile dosya yolu seçme
+            //using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            //{
+            //    saveFileDialog.Filter = "SQL Server Backup Files (*.bak)|*.bak";
 
-                // Kullanıcı dosya yolunu seçerse
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    string backupFilePath = saveFileDialog.FileName;
+            //    // Kullanıcı dosya yolunu seçerse
+            //    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            //    {
+            //        string backupFilePath = saveFileDialog.FileName;
 
-                    // Seçilen yolu TextBox'a yazdır
-                    textBox1.Text = backupFilePath;
+            //        // Seçilen yolu TextBox'a yazdır
+            //        textBox1.Text = backupFilePath;
 
-                    // Yolu path.txt dosyasına kaydet
-                    SaveBackupPath(backupFilePath);
-                }
-            }
+            //        // Yolu path.txt dosyasına kaydet
+            //        SaveBackupPath(backupFilePath);
+            //    }
+            //}
         }
         private void BackupDatabase()
         {
-            string backupFilePath = textBox1.Text + "\\yedek_" + DateTime.Now.ToString("dd.MM.yyyy") + ".bak";
+            
+            string backupFilePath = textBox1.Text + "\\Backup" + DateTime.UtcNow.Ticks +".bak";
 
             if (string.IsNullOrEmpty(backupFilePath))
             {
